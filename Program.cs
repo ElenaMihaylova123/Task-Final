3bff38add9a09522791dc6890b26bee825bba73d
ElenaMihaylova <elena.16.04@mail.ru>
2023-06-24 14:10:57 +0700

    Add main fucntional part

--- Program.cs
+++ Program.cs
@@ -1,2 +1,15 @@
-﻿string[] array1 = new string[5] {"123", "23", "hello", "world", "res"};
-string[] array2 = new string[array1.Length];
+﻿string[] source = new string[4] {"Hello", "2", "world", ":-)"};
+string[] result = new string[source.Length];
+
+void SecondArrayWithIF(string[] array1, string[] array2)
+{
+    int count = 0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+        if(array1[i].Length <= 3)
+        {
+            array2[count] = array1[i];
+            count++;
+        }
+    }
+}
